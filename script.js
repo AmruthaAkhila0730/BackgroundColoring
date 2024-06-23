215969c0369ec03b69efc6b12e74a83b0fb55892
AmruthaAkhila0730 <152167700+AmruthaAkhila0730@users.noreply.github.com>
2024-06-23 18:45:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function change1() {
+    document.body.style.backgroundColor = "red";
+}
+function change2() {
+    document.body.style.backgroundColor = "blue";
+}
+function change3() {
+    document.body.style.backgroundColor = "green";
+}
+function change4() {
+    document.body.style.backgroundColor = "";
+}
